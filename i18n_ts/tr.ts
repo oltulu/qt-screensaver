62047283f6816b1a30f1b6baa8a086c366bbc1a9
Aylinux <cihanalk@gmail.com>
2018-08-08 10:01:32 +0300

    Create tr.ts

---
+++ i18n_ts/tr.ts
@@ -0,0 +1,84 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr_TR">
+<context>
+    <name>BioDeviceView</name>
+    <message>
+        <location filename="../src/bioAuthentication/biodeviceview.cpp" line="207"/>
+        <source>password login</source>
+        <translation>Giriş Şifresi</translation>
+    </message>
+    <message>
+        <location filename="../src/bioAuthentication/biodeviceview.cpp" line="213"/>
+        <source>fingerprint</source>
+        <translation>Parmak izi</translation>
+    </message>
+    <message>
+        <location filename="../src/bioAuthentication/biodeviceview.cpp" line="216"/>
+        <source>fingerevin</source>
+        <translation>fingerevin</translation>
+    </message>
+    <message>
+        <location filename="../src/bioAuthentication/biodeviceview.cpp" line="219"/>
+        <source>iris</source>
+        <translation>iris</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="124"/>
+        <source>Avatar</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="180"/>
+        <location filename="../src/mainwindow.ui" line="193"/>
+        <source>TextLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="72"/>
+        <source>Username</source>
+        <translation>Kullanıcı Adı</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="111"/>
+        <source>Has Logged In</source>
+        <translation>Giriş Yapıldı</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="98"/>
+        <source>Unlock</source>
+        <translation>Kilit Açık</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="424"/>
+        <source>Password Incorrect</source>
+        <translation>Parola Hatalı</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../src/main.cpp" line="30"/>
+        <source>Dialog for the ukui ScreenSaver.</source>
+        <translation>Ukui ScreenSaver için iletişim kutusu.</translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="35"/>
+        <source>lock the screen immediately</source>
+        <translation>Ekranı hemen kilitle</translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="37"/>
+        <source>show saver</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
